eb6f0332aff06ef4907ef6a8449d201b0461fe59
krokben <tombookpro@Tombooks-MacBook-Pro.local>
2017-03-22 11:59:35 +0100

    css and some backend

--- src/components/CardPool.js
+++ src/components/CardPool.js
@@ -7,6 +7,7 @@ export default class CardPool extends Component {
 	render() {
 		return (
 			<div className="CardPool_container">
+				<h4>Cardpool</h4>
 				<UserStories location="cardpool" cards={this.props.cards} choose={this.props.choose} />
 				<MaintenanceCards location="cardpool" cards={this.props.cards} choose={this.props.choose} />
 				<DefectCards location="cardpool" cards={this.props.cards} choose={this.props.choose} />
